ed25fac7933f8f390017c9a3979edcbca7e3d190
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-03-18 09:44:51 +0100

    add task solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,17 @@ function correctionOfForm() {
   const inputCollections = document.querySelectorAll('input');
 
   inputCollections.forEach((inp) => {
+    const textLabel = inp.getAttribute('name');
+
     inp.insertAdjacentHTML(
       'beforebegin',
-      `<label for="${inp.id}" class="field-label">${inp.name}</label>`,
+      `<label for="${inp.id}" class="field-label"></label>`,
     );
+
+    const label = inp.previousElementSibling;
+
+    label.textContent = textLabel;
+
     inp.setAttribute('placeholder', `${inp.name}`);
   });
 }
