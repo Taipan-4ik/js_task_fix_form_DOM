39e938d3fa74839eb927b6ff6ad9574e043b1149
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-03-18 09:29:09 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write code here
+function correctionOfForm() {
+  const inputCollections = document.querySelectorAll('input');
+
+  inputCollections.forEach((inp) => {
+    inp.insertAdjacentHTML(
+      'beforebegin',
+      `<label for="${inp.id}" class="field-label">${inp.name}</label>`,
+    );
+    inp.setAttribute('placeholder', `${inp.name}`);
+  });
+}
+correctionOfForm();
